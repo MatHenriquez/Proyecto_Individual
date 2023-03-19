fbc132ddfd4fbe41af54915f1ea3f6e73de57c50
trechi <matydavila_77@hotmail.com>
2023-03-19 15:01:51 -0300

    Terminada la landing page.

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,5 +1,13 @@
+import styles from '../styles/landing.module.css';
+import { Link} from "react-router-dom";
+
 export default function Landing(){
-    return <div>
-        <h1>Soy la LandingPage</h1>
+
+    return <div className={styles.background}>
+        <h1 className={styles.title}>¡Bienvenidos a la Aventura!</h1>
+        <Link to='/home'>
+         <button className={styles.btn}>HOME</button>
+        </Link>
+        
     </div>
 }
--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,3 +1,5 @@
 export default function NotFound(){
-    return <div></div>
+    return <div>
+        <h1>Not found</h1>
+    </div>
 }
